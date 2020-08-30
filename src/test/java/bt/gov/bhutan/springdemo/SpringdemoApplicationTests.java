20f0566ee0cabb9de66ef8b0e0a00d30a3f4e655
ngawang57 <49856993+ngawang57@users.noreply.github.com>
2020-08-30 14:06:59 +0600

    first commit with application

---
+++ src/test/java/bt/gov/bhutan/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.gov.bhutan.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
